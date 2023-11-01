e187a5c03629432ea1e3083468d0e887fd03dba4
Luke <ldv432@gmail.com>
2023-11-01 12:04:50 -0600

    made some conditionals

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(x){
+  let result
+  if (x <= 400) {
+    result = 'This one is on me!';
+    return result
+  } else if (400 < x && x < 2000) { 
+    result = 'That will be twenty bucks.';
+    return result
+  }
+    else if (x > 2000 && x < 2500) {
+      result = 'I will gladly take your thirty bucks.';
+      return result
+    }
+    else if (x > 2500){
+      result = 'No can do.';
+      return result
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipGood){
+  switch(tipGood){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+        default:
+          return 'Bye.'
+  } 
 }
